eae7358b7ed7f2222e3250ed6515eff5454ac3d0
ggoma5879 <ggoma5879@gmail.com>
2016-03-28 13:14:32 +0900

    w4MAIN.py
    size=100
    angle=90
    turn=20
    bangbangbang(size, angle, turn)

---
+++ w4MAIN.py
@@ -0,0 +1,45 @@
+
+# coding: utf-8
+
+# In[156]:
+
+import turtle
+wn=turtle.Screen()
+t1=turtle.Turtle()
+
+
+# In[162]:
+
+size= 0
+angle= 0
+i=0
+turn=0
+
+
+# In[163]:
+
+def bangbangbang(size,angle,turn):
+        for i in range(turn):
+            if i%2==0:
+                size=size+20
+                
+            else : 
+                size
+            t1.fd(size)
+            t1.right(angle)
+
+
+# In[161]:
+
+bangbangbang(100,90,20)
+
+
+# In[152]:
+
+wn.exitonclick()
+
+
+# In[ ]:
+
+
+
