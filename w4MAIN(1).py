582ee5c049040472f3217e3b302a56fe3f37a2dc
ggoma5879 <ggoma5879@gmail.com>
2016-03-28 13:48:14 +0900

    Update w4MAIN(1).py
    bangbangbang(size,angle,bigger,sides)

--- w4MAIN(1).py
+++ w4MAIN(1).py
@@ -21,7 +21,7 @@ bigger=10
 
 def bangbangbang(size,angle,bigger,sides):
         for i in range(0,sides):
-            if i%2:
+            if i%2==0:
                 size
             else : 
                 size=size+bigger
