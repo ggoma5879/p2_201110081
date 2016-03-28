5c1c47b22f631067c974e6955fbf154319cf8aa6
ggoma5879 <ggoma5879@gmail.com>
2016-03-28 13:42:33 +0900

    4weeks homeworks
    bangbangbang(size,angle,bigger,sides)

---
+++ w4MAIN(1).py
@@ -0,0 +1,51 @@
+
+# coding: utf-8
+
+# In[176]:
+
+import turtle
+wn=turtle.Screen()
+t1=turtle.Turtle()
+
+
+# In[177]:
+
+size= 0
+angle= 0
+i=0
+sides=0
+bigger=10
+
+
+# In[184]:
+
+def bangbangbang(size,angle,bigger,sides):
+        for i in range(0,sides):
+            if i%2==0:
+                size
+            else : 
+                size=size+bigger
+            t1.fd(size)
+            t1.right(angle)
+
+
+# In[185]:
+
+bangbangbang(100,72,10,20)
+
+
+# In[ ]:
+
+wn.exitonclick()
+
+
+# In[183]:
+
+t1.home()
+t1.clear()
+
+
+# In[ ]:
+
+
+
